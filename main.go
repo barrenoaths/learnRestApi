d1004b64aa8746ba49f1cc2a4696bfa734b8392f
barrenoaths <barrenoaths@proton.me>
2026-03-10 19:41:36 +0100

    added basic API testing

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 
+	"learnRestApi/models"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,10 +12,25 @@ func main() {
 	server := gin.Default() // creates web server with basic functionality
 
 	server.GET("/events", getEvents) // handler for inconming http requests (get, post, put, patch, delete)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080") // starts this server on localhost:8080
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
 }
--- models/event.go
+++ models/event.go
@@ -1,1 +1,23 @@
 package models
+
+import "time"
+
+type Event struct {
+	ID          int
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserID      int
+}
+
+var events []Event = []Event{} // do zmiennej events typu []Event przypisujemy pusty slice type []Events
+
+func (e Event) Save() {
+	// later: add it to a database
+	events = append(events, e)
+}
+
+func GetAllEvents() []Event {
+	return events
+}
