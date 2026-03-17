79321a87d4c01022b03dec92018fc59320a6ba65
barrenoaths <barrenoaths@proton.me>
2026-03-17 20:01:15 +0100

    added queries to the database

--- db/db.go
+++ db/db.go
@@ -2,11 +2,42 @@ package db
 
 import (
 	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
+	"fmt"
+
+	_ "modernc.org/sqlite"
 )
 
-var DB
+var DB *sql.DB
 
 func InitDB() {
-	sql.Open("sqlite3", "api.db")
+	var err error
+	DB, err = sql.Open("sqlite", "api.db")
+
+	if err != nil {
+		panic("Could not connect to database.")
+	}
+
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+
+	createTables()
+}
+
+func createTables() {
+	createEventsTable := `
+		CREATE TABLE IF NOT EXISTS events (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+			description TEXT NOT NULL,
+			location TEXT NOT NULL,
+			dateTime DATETIME NOT NULL,
+			user_id INTEGER
+		)
+	`
+
+	_, err := DB.Exec(createEventsTable)
+
+	if err != nil {
+		fmt.Println("The error is: ", err)
+	}
 }
--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"net/http"
 
+	"learnRestApi/db"
 	"learnRestApi/models"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default() // creates web server with basic functionality
 
 	server.GET("/events", getEvents) // handler for inconming http requests (get, post, put, patch, delete)
@@ -20,7 +22,11 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events. Try again later."})
+		return
+	}
 	context.JSON(http.StatusOK, events)
 }
 
@@ -35,7 +41,12 @@ func createEvent(context *gin.Context) {
 	event.ID = 1
 	event.UserID = 1
 
-	event.Save()
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event. Try again later."})
+		return
+	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
 }
