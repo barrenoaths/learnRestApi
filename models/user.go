f78735fc97a6e3b10dc3bb42a332479ed0c5ce91
barrenoaths <barrenoaths@proton.me>
2026-04-07 18:48:14 +0200

    added password hashing

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "learnRestApi/db"
+import (
+	"learnRestApi/db"
+	"learnRestApi/utils"
+)
 
 type User struct {
 	ID       int64
@@ -16,7 +19,14 @@ func (u User) Save() error {
 	}
 
 	defer stmt.Close()
-	result, err := stmt.Exec(u.Email, u.Password)
+
+	hashedPassword, err := utils.HashPassword(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 
 	if err != nil {
 		return err
